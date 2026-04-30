0b496ce85a8facb8e3bb7f4525aeafa3b2b273fe
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2026-04-30 18:13:00 +0300

    fixed wrong snapshot initialization

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/service/SnapshotAggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/service/SnapshotAggregator.java
@@ -19,10 +19,17 @@ public class SnapshotAggregator {
     public Optional<SensorsSnapshotAvro> updateState(SensorEventAvro event) {
 
         if (!snapshots.containsKey(event.getHubId())) {
+            Map<String, SensorStateAvro> stateAvroMap = new HashMap<>();
+
+            stateAvroMap.put(event.getId(), SensorStateAvro.newBuilder()
+                            .setTimestamp(event.getTimestamp())
+                            .setData(event.getPayload())
+                            .build());
+
             SensorsSnapshotAvro snapshot = SensorsSnapshotAvro.newBuilder()
                     .setHubId(event.getHubId())
                     .setTimestamp(event.getTimestamp())
-                    .setSensorsState(new HashMap<>())
+                    .setSensorsState(stateAvroMap)
                     .build();
 
             snapshots.put(snapshot.getHubId(), snapshot);
@@ -54,4 +61,62 @@ public class SnapshotAggregator {
             return Optional.of(existingSnapshot);
         }
     }
+
+
+//    private final Map<String, SensorsSnapshotAvro> snapshots = new HashMap<>();
+//
+//    public Optional<SensorsSnapshotAvro> updateState(SensorEventAvro event) {
+//        String hubId = event.getHubId();
+//
+//        if (!snapshots.containsKey(hubId)) {
+//            SensorsSnapshotAvro snapshot = createNewSnapshot(event);
+//
+//            snapshots.put(hubId, snapshot);
+//
+//            return Optional.of(snapshot);
+//        } else {
+//            SensorsSnapshotAvro oldSnapshot = snapshots.get(hubId);
+//            Optional<SensorsSnapshotAvro> updatedSnapshotOpt = updateSnapshot(oldSnapshot, event);
+//            updatedSnapshotOpt.ifPresent(sensorsSnapshotAvro -> snapshots.put(hubId, sensorsSnapshotAvro));
+//            return updatedSnapshotOpt;
+//        }
+//    }
+//
+//    private Optional<SensorsSnapshotAvro> updateSnapshot(SensorsSnapshotAvro oldSnapshot, SensorEventAvro event) {
+//        String sensorId = event.getId();
+//
+//        if (oldSnapshot.getSensorsState().containsKey(sensorId)) {
+//            if (oldSnapshot.getSensorsState().get(sensorId).getTimestamp().isAfter(event.getTimestamp()) ||
+//                    oldSnapshot.getSensorsState().get(sensorId).getData().equals(event.getPayload())) {
+//                return Optional.empty();
+//            }
+//        }
+//        SensorStateAvro sensorState = createSensorState(event);
+//
+//        oldSnapshot.getSensorsState().put(sensorId, sensorState);
+//        oldSnapshot.setTimestamp(event.getTimestamp());
+//
+//        return Optional.of(oldSnapshot);
+//    }
+//
+//    private SensorsSnapshotAvro buildSnapshot(SensorEventAvro event, Map<String, SensorStateAvro> states) {
+//        return SensorsSnapshotAvro.newBuilder()
+//                .setHubId(event.getHubId())
+//                .setTimestamp(event.getTimestamp())
+//                .setSensorsState(states)
+//                .build();
+//    }
+//
+//    private SensorStateAvro createSensorState(SensorEventAvro event) {
+//        return SensorStateAvro.newBuilder()
+//                .setTimestamp(event.getTimestamp())
+//                .setData(event.getPayload())
+//                .build();
+//    }
+//
+//    private SensorsSnapshotAvro createNewSnapshot(SensorEventAvro event) {
+//        Map<String, SensorStateAvro> sensorStates = new HashMap<>();
+//        sensorStates.put(event.getId(), createSensorState(event));
+//        return buildSnapshot(event, sensorStates);
+//    }
 }
